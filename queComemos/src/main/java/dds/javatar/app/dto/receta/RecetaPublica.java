085cce32fa3d8c3a19e259702ea6547def94748f
anicolasgar <a.nicolasgar@gmail.com>
2015-05-31 12:39:46 -0300

    Saque imports al pedo

--- queComemos/src/main/java/dds/javatar/app/dto/receta/RecetaPublica.java
+++ queComemos/src/main/java/dds/javatar/app/dto/receta/RecetaPublica.java
@@ -1,8 +1,5 @@
 package dds.javatar.app.dto.receta;
 
-import dds.javatar.app.dto.usuario.Usuario;
-import dds.javatar.app.util.exception.RecetaException;
-import dds.javatar.app.util.exception.UsuarioException;
 
 public interface RecetaPublica extends Receta{
 	

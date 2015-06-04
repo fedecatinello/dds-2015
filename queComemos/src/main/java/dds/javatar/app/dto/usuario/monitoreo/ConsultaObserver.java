46ff42fed3a6624441d118b0d0311fba2122f9e8
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2015-06-03 23:10:51 -0300

    added TODO.

--- queComemos/src/main/java/dds/javatar/app/dto/usuario/monitoreo/ConsultaObserver.java
+++ queComemos/src/main/java/dds/javatar/app/dto/usuario/monitoreo/ConsultaObserver.java
@@ -5,6 +5,8 @@ import dds.javatar.app.dto.usuario.Usuario;
 
 public interface ConsultaObserver {
 
+	
+	// TODO: cambiar los parametros a un objeto.
 	void notificarConsulta(Usuario usuario, String nombre, Dificultad dificultad);
 	
 }

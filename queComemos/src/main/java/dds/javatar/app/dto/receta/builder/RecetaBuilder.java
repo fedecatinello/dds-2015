7fb3cbc6d16bce72067f450224d356704f0a1e8d
fcatinello <fcatinello@gmail.com>
2015-08-16 22:14:49 -0300

    arreglando builders

--- queComemos/src/main/java/dds/javatar/app/dto/receta/builder/RecetaBuilder.java
+++ queComemos/src/main/java/dds/javatar/app/dto/receta/builder/RecetaBuilder.java
@@ -1,22 +1,29 @@
 package dds.javatar.app.dto.receta.builder;
 
 import java.math.BigDecimal;
-import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import dds.javatar.app.dto.receta.Receta;
-import dds.javatar.app.dto.receta.RecetaPrivada;
 import dds.javatar.app.dto.receta.RecetaPrivadaCompuesta;
+import dds.javatar.app.dto.receta.RecetaPrivadaSimple;
+import dds.javatar.app.dto.receta.RecetaPublicaCompuesta;
+import dds.javatar.app.dto.receta.RecetaPublicaSimple;
 import dds.javatar.app.util.exception.RecetaException;
 
-public abstract class RecetaBuilder {
+public class RecetaBuilder {
 
 	private String nombre;
-	private String temporada;
-	private Integer calorias;
-	private String dificultad;
-	private Integer tiempoPreparacion;
-	
-	private Receta receta;
+    private String dificultad;
+    private Integer calorias;
+    private String temporada;
+    private Integer tiempoPreparacion;
+    private String autor;
+    private HashMap<String, BigDecimal> condimentos;
+    private HashMap<String, BigDecimal> ingredientes;
+    private HashMap<Integer, String> pasosPreparacion;
+    private HashSet<Receta> subrecetas;
+    
 	
 	public RecetaBuilder nombre(String nombre) {
 		this.nombre = nombre;
@@ -29,7 +36,7 @@ public abstract class RecetaBuilder {
 	}
 
 	public RecetaBuilder totalCalorias(Integer calorias) {
-		this.calorias = calorias;
+		this.calorias = calorias ;
 		return this;
 	}
 
@@ -42,8 +49,58 @@ public abstract class RecetaBuilder {
 		this.tiempoPreparacion = tiempoPreparacion;
 		return this;
 	}
-
-	public RecetaBuilder buildReceta() {
-		return recetaPrivada;
+	
+	public RecetaBuilder inventadaPor(String autor) {
+		this.autor = autor;
+		return this;
 	}
+
+	public RecetaBuilder agregarCondimentos(String condimento, BigDecimal cantidad) {
+		this.condimentos.put(condimento, cantidad);
+		return this;
+	}
+
+	public RecetaBuilder agregarIngrediente(String ingrediente, BigDecimal cantidad) {
+		this.ingredientes.put(ingrediente, cantidad);
+		return this;
+	}
+
+	public RecetaBuilder agregarPaso(Integer nroPaso, String pasoPreparacion) {
+		this.pasosPreparacion.put(nroPaso, pasoPreparacion);
+		return this;
+	}
+	
+	public RecetaBuilder agregarSubReceta(Receta subReceta) throws RecetaException {
+		this.subrecetas.add(subReceta);
+		return this;
+	}
+	
+	public Receta buildReceta() {
+		
+		if(this.autor.isEmpty()) { /** Receta Publica **/			
+			
+			if(esCompuesta())
+				return new RecetaPublicaCompuesta();
+			else
+				return new RecetaPublicaSimple();
+		}	
+		else {                    /** Receta Privada **/
+			
+			if(esCompuesta())
+				return new RecetaPrivadaCompuesta();
+			else
+				return new RecetaPrivadaSimple();
+		}
+			
+			
+	}
+	
+	public Boolean esCompuesta() {
+		
+		if(this.subrecetas.isEmpty()) 
+			return true;
+		else
+			return false;
+	}
+
 }

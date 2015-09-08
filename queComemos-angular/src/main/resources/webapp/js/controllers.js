9ad24ecb0e76ef7bf84f4edb181f6f505c6c3ce0
fcatinello <fcatinello@gmail.com>
2015-09-08 12:20:49 -0300

    corrigiendo algunas cosas

--- queComemos-angular/src/main/resources/webapp/js/controllers.js
+++ queComemos-angular/src/main/resources/webapp/js/controllers.js
@@ -7,15 +7,18 @@ var app = angular.module('queComemosApp', []);
 app.controller('RecetasController', function(recetasService) {
 	var self = this;
 	self.recetas = [];
+	self.mensajeInicio = 'lalalal';
 
-	function transformarAReceta(jsonTarea) {
-		return Receta.asReceta(jsonTarea);
+	function transformarAReceta(jsonReceta) {
+		//return Receta.asReceta(jsonTarea);
+		return jsonReceta.parse();
 	}
 
 	this.getRecetas = function() {
 		recetasService.findAll(function(data) {
 			self.recetas = _.map(data, transformarAReceta);
 		});
+		return self.recetas;
 	}
 
 });
--- queComemos-angular/src/main/resources/webapp/js/receta.js
+++ queComemos-angular/src/main/resources/webapp/js/receta.js
@@ -1,5 +1,9 @@
 var Receta = function() {
-
+    this.nombre = null;
+    this.dificultad = null;
+    this.temporada = null;
+    this.calorias = 0;
+    this.autor = null;
 };
 
 Receta.prototype.cumplir = function() {

3505842cd09e90e87a5c01644729c39fe002b572
fcatinello <fcatinello@gmail.com>
2015-09-07 23:24:26 -0300

    agrego controller de recetas

---
+++ queComemos/src/main/java/dds/javatar/app/dto/receta/util/transformer/JsonTransformer.java
@@ -0,0 +1,19 @@
+package dds.javatar.app.dto.receta.util.transformer;
+
+import spark.ResponseTransformer;
+import com.google.gson.Gson;
+
+public class JsonTransformer implements ResponseTransformer {
+
+	private Gson gson;
+
+	public JsonTransformer(Gson gson) {
+		this.gson = gson;
+	}
+
+	@Override
+	public String render(Object model) {
+		return this.gson.toJson(model);
+	}
+
+}

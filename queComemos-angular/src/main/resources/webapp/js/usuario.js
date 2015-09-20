251b388f385c7fca8ef6ba534ad91afda10afcda
ElianaLS <eliana.lugueros@hotmail.com>
2015-09-20 17:57:12 -0300

    Agrego filtros de color por IMC: .css, .js, .html

--- queComemos-angular/src/main/resources/webapp/js/usuario.js
+++ queComemos-angular/src/main/resources/webapp/js/usuario.js
@@ -7,6 +7,27 @@ var Usuario = function() {
     this.password = null;
 };
 
+var Perfil = function() {
+    this.nombre = null;
+    this.complexion = null;
+    this.sexo = null;
+    this.edad = null;
+    this.fechaNacimiento = null;
+    this.altura = null;
+    this.peso = null;
+    this.imc = null;
+};
+
 Usuario.asUsuario = function (jsonUsuario) {
     return angular.extend(new Usuario(), jsonUsuario);
+};
+
+Perfil.prototype.esAlto() = function() {
+    return this.imc>30;
+};
+Perfil.prototype.esBajo() = function() {
+    return this.imc<18;
+};
+Perfil.prototype.esMedio() = function() {
+    return this.imc>=18 && this.imc<=30;
 };

165b39f94ce9eb6db225c3f6662057c13a33b255
Nicolas Garcia <a.nicolasgar@gmail.com>
2015-09-23 10:44:41 -0300

    Saco service repetido

--- queComemos-angular/src/main/resources/webapp/servicesUsuario.js
+++
@@ -1,6 +0,0 @@
-usuariosModule.service('usuarioService', function($http) {
-
-	this.getUserInfoByUsername = function(username, callback) {
-		$http.get('/usuarios/' + username).success(callback);
-	};
-});
